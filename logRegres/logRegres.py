1abf824631abca615c3a34b101aa599da7284335
liuyuzhe <liuyuzhe110@qq.com>
2017-09-23 12:20:20 +0800

    logistic回归拟合直线的绘制

--- logRegres/logRegres.py
+++ logRegres/logRegres.py
@@ -14,9 +14,11 @@ def loadDataSet():
     return dataMat, labelMat
 
 def sigmoid(inX):
+    """阶跃函数"""
     return 1.0 / (1 + exp(-inX))
 
 def gradAscent(dataMatIn, classLabels):
+    """梯度上升算法"""
     dataMatrix = mat(dataMatIn)
     labelMat = mat(classLabels).transpose()  # 矩阵转置，得到列向量
     m, n = shape(dataMatrix)  # 矩阵维数
@@ -30,8 +32,43 @@ def gradAscent(dataMatIn, classLabels):
         weights = weights + aplha * dataMatrix.transpose() * error
     return weights
 
-# """
+"""
 dataArr, labelMat = loadDataSet()
 ret = gradAscent(dataArr, labelMat)
 print ret
+"""
+
+def plotBestFit(weights):
+    """画出数据集和Logistic回归最佳拟合直线的函数"""
+    import matplotlib.pyplot as plt
+    dataMat, labelMat = loadDataSet()
+    dataArray = array(dataMat)  # list转numpy.array
+    n = shape(dataArray)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArray[i, 1])
+            ycord1.append(dataArray[i, 2])
+        else:
+            xcord2.append(dataArray[i, 1])
+            ycord2.append(dataArray[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)  # 画布分割为1行1列，图像画在第一块
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')  # 绘制散点图
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)  # 等差数列，从-3.0到3.0，公差0.1
+    y = (-weights[0] - weights[1] * x) / weights[2]  # 最佳拟合直线
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
 # """
+dataArr, labelMat = loadDataSet()
+weights = gradAscent(dataArr, labelMat)
+weigthsArray = matrix.getA(weights)  # 矩阵转数组 matrix.getA()
+plotBestFit(weigthsArray)
+# """
+
+
