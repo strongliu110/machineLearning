2baf6c057438e902237e484237f11f1547a481da
liuyuzhe <liuyuzhe110@qq.com>
2017-11-15 23:45:38 +0800

    局部加权线性回归函数

--- regression/regression.py
+++ regression/regression.py
@@ -35,7 +35,7 @@ def standRegres(xArr, yArr):
     # ws = linalg.solve(xTx, xMat.T * yMat)
     return ws
 
-# """
+"""
 xArr, yArr = loadDataSet('ex0.txt')
 ws = standRegres(xArr, yArr)
 
@@ -56,5 +56,54 @@ plt.show()
 
 yHat = xMat * ws
 print corrcoef(yHat.T, yMat)  # 相关系数
+
+"""
+
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    """
+    局部加权线性回归函数
+    :param testPoint:
+    :param xArr:
+    :param yArr:
+    :param k:
+    :return:
+    """
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    weights = mat(eye((m)))  # 创建对角矩阵
+    for j in range(m):
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = exp(diffMat * diffMat.T / (-2.0 * k ** 2))  # 权值值以指数级衰减
+    xTx = xMat.T * (weights * xMat)
+    if linalg.det(xTx) == 0.0:
+        print "This matrix is singular, cannot do inverse"
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    return yHat
+
+# """
+xArr, yArr = loadDataSet('ex0.txt')
+lwlr(xArr[0], xArr, yArr, 1.0)
+lwlr(xArr[0], xArr, yArr, 0.001)
+yHat = lwlrTest(xArr, xArr, yArr, 0.003)
+
+xMat = mat(xArr)
+srtInd = xMat[:, 1].argsort(0)
+xSort = xMat[srtInd][:, 0, :]
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(xSort[:, 1], yHat[srtInd])
+ax.scatter(xMat[:, 1].flatten().A[0], mat(yArr).T.flatten().A[0], s=2, c="red")
+plt.show()
 
 # """
