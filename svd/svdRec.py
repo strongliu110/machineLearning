f36ea29ffb1fc7c46fb74e111b9c1b30a078095a
liuyuzhe <liuyuzhe110@qq.com>
2017-11-24 23:39:09 +0800

    利用SVD简化数据，推荐系统

---
+++ svd/svdRec.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+
+"""
+利用SVD来简化数据
+"""
+
+from numpy import *
+from numpy import linalg as la
+
+def loadExData():
+    return[[0, 0, 0, 2, 2],
+           [0, 0, 0, 3, 3],
+           [0, 0, 0, 1, 1],
+           [1, 1, 1, 0, 0],
+           [2, 2, 2, 0, 0],
+           [5, 5, 5, 0, 0],
+           [1, 1, 1, 0, 0]]
+
+"""
+Data = loadExData();
+U, Sigma, VT = la.svd(Data)
+print "Sigma = ", Sigma
+Sig3 = mat([[Sigma[0], 0, 0], [0, Sigma[1], 0], [0, 0, Sigma[2]]])
+print U[:, :3] * Sig3 * VT[:3, :]
+"""
+
+def ecludSim(inA,inB):
+    """
+    欧式距离
+    :param inA:
+    :param inB:
+    :return:
+    """
+    return 1.0 / (1.0 + la.norm(inA - inB))
+
+def pearsSim(inA,inB):
+    """
+    皮尔逊相似度
+    :param inA:
+    :param inB:
+    :return:
+    """
+    if len(inA) < 3:
+        return 1.0
+    return 0.5 + 0.5 * corrcoef(inA, inB, rowvar=0)[0][1]
+
+def cosSim(inA, inB):
+    """
+    余弦相似度
+    :param inA:
+    :param inB:
+    :return:
+    """
+    num = float(inA.T * inB)
+    denom = la.norm(inA) * la.norm(inB)
+    return 0.5 + 0.5 * (num / denom)
+
+
+def standEst(dataMat, user, simMeas, item):
+    """
+    基于协同过滤的评分估计
+    :param dataMat:
+    :param user:
+    :param simMeas:
+    :param item:
+    :return:
+    """
+    n = shape(dataMat)[1]
+    simTotal = 0.0; ratSimTotal = 0.0
+    for j in range(n):
+        userRating = dataMat[user, j]
+        if userRating == 0:
+            continue
+        overLap = nonzero(logical_and(dataMat[:, item].A > 0, dataMat[:, j].A > 0))[0]  # 寻找两个用户都评级的物品
+        if len(overLap) == 0:
+            similarity = 0
+        else:
+            similarity = simMeas(dataMat[overLap, item], dataMat[overLap, j])
+        print 'the %d and %d similarity is: %f' % (item, j, similarity)
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+    if simTotal == 0:
+        return 0
+    else:
+        return ratSimTotal/simTotal
+
+
+def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
+    """
+    推荐算法
+    :param dataMat:
+    :param user:
+    :param N:
+    :param simMeas:
+    :param estMethod:
+    :return:
+    """
+    unratedItems = nonzero(dataMat[user, :].A == 0)[1]  # 寻找未评级的物品
+    if len(unratedItems) == 0: return 'you rated everything'
+    itemScores = []
+    for item in unratedItems:
+        estimatedScore = estMethod(dataMat, user, simMeas, item)
+        itemScores.append((item, estimatedScore))
+    return sorted(itemScores, key=lambda jj: jj[1], reverse=True)[:N]
+
+"""
+myMat = mat(loadExData())
+myMat[0,1] = myMat[0,0] = myMat[1,0] = myMat[2,0] = 4
+recommend(myMat, 2)
+recommend(myMat, 2, simMeas=ecludSim)
+recommend(myMat, 2, simMeas=pearsSim)
+"""
+
+
+def loadExData2():
+    return [[0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 5],
+            [0, 0, 0, 3, 0, 4, 0, 0, 0, 0, 3],
+            [0, 0, 0, 0, 4, 0, 0, 1, 0, 4, 0],
+            [3, 3, 4, 0, 0, 0, 0, 2, 2, 0, 0],
+            [5, 4, 5, 0, 0, 0, 0, 5, 5, 0, 0],
+            [0, 0, 0, 0, 5, 0, 1, 0, 0, 5, 0],
+            [4, 3, 4, 0, 0, 0, 0, 5, 5, 0, 1],
+            [0, 0, 0, 4, 0, 4, 0, 0, 0, 0, 4],
+            [0, 0, 0, 2, 0, 2, 5, 0, 0, 1, 2],
+            [0, 0, 0, 0, 5, 0, 0, 0, 0, 4, 0],
+            [1, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0]]
+
+"""
+U, Sigma, VT = la.svd(mat(loadExData2()))
+Sig2 = Sigma ** 2
+sum(Sig2) * 0.9
+"""
+
+
+def svdEst(dataMat, user, simMeas, item):
+    """
+    基于SVD的评分估计
+    :param dataMat:
+    :param user:
+    :param simMeas:
+    :param item:
+    :return:
+    """
+    n = shape(dataMat)[1]
+    simTotal = 0.0
+    ratSimTotal = 0.0
+    U, Sigma, VT = la.svd(dataMat)
+    Sig4 = mat(eye(4) * Sigma[:4])  # arrange Sig4 into a diagonal matrix
+    xformedItems = dataMat.T * U[:, :4] * Sig4.I  # create transformed items
+    for j in range(n):
+        userRating = dataMat[user, j]
+        if userRating == 0 or j == item: continue
+        similarity = simMeas(xformedItems[item, :].T, xformedItems[j, :].T)
+        print 'the %d and %d similarity is: %f' % (item, j, similarity)
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+    if simTotal == 0:
+        return 0
+    else:
+        return ratSimTotal / simTotal
+
+"""
+myMat = mat(loadExData())
+recommend(myMat, 1, estMethod=svdEst)
+recommend(myMat, 1, estMethod=svdEst, simMeas=pearsSim)
+"""
